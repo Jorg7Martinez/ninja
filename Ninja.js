09a44cdda96d6a99321fc45a5534f526b78398e3
Jorg7Mr <jorgemf195@gmail.com>
2024-04-07 12:03:23 -0400

    agregando clase sensei

--- Ninja.js
+++ Ninja.js
@@ -24,3 +24,31 @@ class Ninja {
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+  constructor(
+    nombre,
+    salud = 200,
+    velocidad = 10,
+    fuerza = 10,
+    sabiduria = 10
+  ) {
+    super(nombre, salud, velocidad, fuerza);
+    this.sabiduria = sabiduria;
+  }
+
+  speakWisdom() {
+    super.drinkSake();
+    console.log("La verdadera sabiduría viene de la experiencia.");
+  }
+
+  drinkSake() {
+    console.log(`${this.nombre} ha bebido sake y ha aumentado su salud.`);
+  }
+}
+
+const sensei1 = new Sensei("Spike");
+sensei1.sayName();
+sensei1.showStats();
+sensei1.speakWisdom();
+sensei1.showStats();
